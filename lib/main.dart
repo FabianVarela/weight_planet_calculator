f0518c9b39ec476cf53b808f83e9004d3a5ad892
FabianVarela <jfabian.303@gmail.com>
2020-04-03 22:48:02 -0500

    Set theme in main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:weight_planet_calculator/ui/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Weight Planet Calculator",
-    home: Home(),
-  ));
+  runApp(
+    MaterialApp(
+      title: 'Weight Planet Calculator',
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+        backgroundColor: Colors.white,
+      ),
+      home: Home(),
+    ),
+  );
 }
