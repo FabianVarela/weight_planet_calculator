dca88e328bd9a8ce8a5800aefaea029a082c0a16
FabianVarela <jfabian.303@gmail.com>
2020-04-03 22:48:19 -0500

    Create custom clip path

---
+++ lib/ui/custom_clippath.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class HeaderClipPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+
+    path.lineTo(0, size.height - 50);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 50);
+
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
