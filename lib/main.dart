bca9fe10a3efb0816798fc641cbe51da0ed15988
FabianVarela <jfabian.303@gmail.com>
2020-04-06 18:19:16 -0500

    Modify main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:weight_planet_calculator/ui/home.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Weight Planet Calculator',
       theme: ThemeData(
         primaryColor: Colors.blueGrey,
         backgroundColor: Colors.white,
       ),
       home: Home(),
-    ),
-  );
+    );
+  }
 }
