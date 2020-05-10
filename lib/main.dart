821d5f0c284f288cb2393cd2132becbab3c4d550
FabianVarela <jfabian.303@gmail.com>
2020-05-10 12:14:13 -0500

    Update main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:weight_planet_calculator/ui/home.ui.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
