f480bbda02c79eae3af75dd08ea66d12edf6cfdb
FabianVarela <jfabian.303@gmail.com>
2021-05-31 20:56:25 -0500

    Change rotation_list_animation.dart as hook widget

--- lib/ui/widgets/rotation_list_animation.dart
+++ lib/ui/widgets/rotation_list_animation.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:weight_planet_calculator/hooks/tween_animation_hook.dart';
 import 'package:weight_planet_calculator/ui/widgets/rotation_item.dart';
 
-class RotationListAnimation extends StatefulWidget {
+class RotationListAnimation extends HookWidget {
   RotationListAnimation({
     required this.size,
     required this.assetList,
@@ -16,81 +18,44 @@ class RotationListAnimation extends StatefulWidget {
   final int currentIndex;
   final bool isReverse;
 
-  @override
-  _RotationListAnimationState createState() => _RotationListAnimationState();
-}
-
-class _RotationListAnimationState extends State<RotationListAnimation>
-    with SingleTickerProviderStateMixin {
-  double get _myHeight => widget.size.height * 0.8; // 0.4
-
-  late AnimationController _controller;
-  late Tween<double> _rotationTween;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration: Duration(milliseconds: 500),
-      vsync: this,
-    )..addListener(() => setState(() {}));
-
-    _rotationTween = Tween<double>(
-      begin: 0.0,
-      end: widget.currentIndex.toDouble(),
-    );
-
-    _controller.forward();
-  }
-
-  @override
-  void didUpdateWidget(RotationListAnimation oldWidget) {
-    if (widget.currentIndex != oldWidget.currentIndex) {
-      _rotationTween = Tween<double>(
-        begin: _rotationTween.evaluate(_controller),
-        end: widget.currentIndex.toDouble(),
-      );
-      _controller.forward(from: 0.0);
-    }
-
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  double get _myHeight => size.height * 0.8; // 0.4
 
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController(
+      duration: Duration(milliseconds: 500),
+    );
+    final tween = useTweenAnimation(
+      animationController,
+      begin: 0.0,
+      end: currentIndex.toDouble(),
+    );
+
     final stackChildren = <Widget>[_Circle(height: _myHeight)];
 
-    for (var i = 0; i < widget.assetList.length; i++) {
+    for (var i = 0; i < assetList.length; i++) {
       final radialOffset = _myHeight / 2;
-      final radianDiff = (2 * pi) / widget.assetList.length;
-      final rotationFactor = _rotationTween.animate(_controller).value;
+      final radianDiff = (2 * pi) / assetList.length;
+      final rotationFactor = tween.animate(animationController).value;
       final startRadian = -pi / 2 + -rotationFactor * radianDiff;
       final radians = startRadian + i * radianDiff;
 
-      final reverseValue = widget.isReverse ? -1 : 1; // original 1
+      final reverseValue = isReverse ? -1 : 1; // original 1
       final dx = radialOffset * (reverseValue * cos(radians));
       final dy = radialOffset * (reverseValue * sin(radians));
 
       stackChildren.add(
         Transform.translate(
           offset: Offset(dx, dy),
-          child: RotationItem(asset: widget.assetList[i]),
+          child: RotationItem(asset: assetList[i]),
         ),
       );
     }
 
     return FractionalTranslation(
-      translation: Offset(0.0, widget.isReverse ? -0.9 : 0.9), // 0.2
+      translation: Offset(0.0, isReverse ? -0.9 : 0.9), // 0.2
       child: Align(
-        alignment:
-            widget.isReverse ? Alignment.topCenter : Alignment.bottomCenter,
+        alignment: isReverse ? Alignment.topCenter : Alignment.bottomCenter,
         child: Container(
           height: _myHeight,
           child: Stack(alignment: Alignment.center, children: stackChildren),
