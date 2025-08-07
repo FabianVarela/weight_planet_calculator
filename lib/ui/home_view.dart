ce422a61d14b86303218400622e2804147631192
FabianVarela <jfabian.303@gmail.com>
2025-08-06 19:59:08 -0500

    Remove import

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:weight_planet_calculator/hooks/curved_animation_hook.dart';
