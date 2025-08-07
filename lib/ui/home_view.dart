ca399fc897d6b6d6c1768812bbcf63b623e65562
FabianVarela <jfabian.303@gmail.com>
2025-08-06 19:56:26 -0500

    Set keyboard dismiss

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:collection/collection.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:weight_planet_calculator/hooks/curved_animation_hook.dart';
@@ -30,6 +31,7 @@ class HomeView extends HookWidget {
 
     return Scaffold(
       body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: Column(
           spacing: 25,
           children: <Widget>[
