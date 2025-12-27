14c04c8516d12ec9ac0643347a8c153f8e6ba20b
FabianVarela <jfabian.303@gmail.com>
2025-12-26 19:30:32 -0500

    Replace hardcoded path with asset gen file

--- lib/model/planet_model.dart
+++ lib/model/planet_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weight_planet_calculator/styles/generated/assets.gen.dart';
 
 class Planet {
   Planet(this.name, this.asset, this.weightGravity, this.color);
@@ -10,13 +11,13 @@ class Planet {
 }
 
 final List<Planet> planets = <Planet>[
-  Planet('Mercury', 'assets/images/mercury.png', 0.38, Colors.red),
-  Planet('Venus', 'assets/images/venus.png', 0.91, Colors.yellow),
-  Planet('Earth', 'assets/images/earth.png', 1, Colors.green),
-  Planet('Mars', 'assets/images/mars.png', 0.38, Colors.blueGrey),
-  Planet('Jupiter', 'assets/images/jupiter.png', 2.34, Colors.black),
-  Planet('Saturn', 'assets/images/saturn.png', 0.93, Colors.orange),
-  Planet('Uranus', 'assets/images/uranus.png', 0.92, Colors.brown),
-  Planet('Neptune', 'assets/images/neptune.png', 1.12, Colors.indigo),
-  Planet('Pluto', 'assets/images/pluto.png', 0.06, Colors.amber),
+  Planet('Mercury', Assets.images.mercury, .38, Colors.red),
+  Planet('Venus', Assets.images.venus, .91, Colors.yellow),
+  Planet('Earth', Assets.images.earth, 1, Colors.green),
+  Planet('Mars', Assets.images.mars, .38, Colors.blueGrey),
+  Planet('Jupiter', Assets.images.jupiter, 2.34, Colors.black),
+  Planet('Saturn', Assets.images.saturn, .93, Colors.orange),
+  Planet('Uranus', Assets.images.uranus, .92, Colors.brown),
+  Planet('Neptune', Assets.images.neptune, 1.12, Colors.indigo),
+  Planet('Pluto', Assets.images.pluto, .06, Colors.amber),
 ];
---
+++ lib/styles/generated/assets.gen.dart
@@ -0,0 +1,60 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/earth.png
+  String get earth => 'assets/images/earth.png';
+
+  /// File path: assets/images/jupiter.png
+  String get jupiter => 'assets/images/jupiter.png';
+
+  /// File path: assets/images/mars.png
+  String get mars => 'assets/images/mars.png';
+
+  /// File path: assets/images/mercury.png
+  String get mercury => 'assets/images/mercury.png';
+
+  /// File path: assets/images/neptune.png
+  String get neptune => 'assets/images/neptune.png';
+
+  /// File path: assets/images/pluto.png
+  String get pluto => 'assets/images/pluto.png';
+
+  /// File path: assets/images/saturn.png
+  String get saturn => 'assets/images/saturn.png';
+
+  /// File path: assets/images/uranus.png
+  String get uranus => 'assets/images/uranus.png';
+
+  /// File path: assets/images/venus.png
+  String get venus => 'assets/images/venus.png';
+
+  /// List of all assets
+  List<String> get values => [
+    earth,
+    jupiter,
+    mars,
+    mercury,
+    neptune,
+    pluto,
+    saturn,
+    uranus,
+    venus,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
