1a71f2ea1145dfabf65208614f0ea50b5252a742
FabianVarela <jfabian.303@gmail.com>
2025-12-26 19:31:29 -0500

    Update custom_radio_button and remove deprecated properties

--- lib/ui/widgets/custom_radio_button.dart
+++ lib/ui/widgets/custom_radio_button.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
 
-class CustomRadioButton extends StatelessWidget {
-  const CustomRadioButton({
-    required this.title,
+enum RadioButtonsPosition { row, column, wrap }
+
+class CustomRadioButtonGroup<T> extends StatelessWidget {
+  const CustomRadioButtonGroup({
+    required this.groupList,
     required this.value,
-    required this.groupValue,
-    required this.weight,
-    required this.color,
+    required this.position,
     this.onChanged,
     super.key,
   });
 
-  final String title;
-  final int value;
-  final int groupValue;
-  final double weight;
-  final Color color;
-  final ValueSetter<int?>? onChanged;
+  final List<({String title, T value, Color color})> groupList;
+  final T? value;
+  final RadioButtonsPosition position;
+  final ValueSetter<T?>? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 5,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Radio(
-          activeColor: color,
-          value: value,
-          groupValue: groupValue,
-          onChanged: (_) => onChanged?.call(value),
-        ),
-        InkWell(
-          onTap: () => onChanged?.call(value),
-          child: Text(
-            title,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w300,
-              color: Theme.of(context).primaryColor,
+    final children = <Widget>[
+      for (final item in groupList)
+        Row(
+          spacing: 5,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Radio(activeColor: item.color, value: item.value),
+            InkWell(
+              onTap: () => onChanged?.call(item.value),
+              child: Text(
+                item.title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: .w300,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      ],
+    ];
+
+    return RadioGroup<T>(
+      groupValue: value,
+      onChanged: (value) => onChanged?.call(value),
+      child: switch (position) {
+        RadioButtonsPosition.row => Row(spacing: 10, children: children),
+        RadioButtonsPosition.column => Column(
+          spacing: 10,
+          crossAxisAlignment: .start,
+          children: children,
+        ),
+        RadioButtonsPosition.wrap => Wrap(
+          alignment: .center,
+          children: children,
+        ),
+      },
     );
   }
 }
