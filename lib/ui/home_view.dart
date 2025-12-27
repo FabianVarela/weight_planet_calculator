ae9cce48b4c3ae85c9e3e73fcf67be7831569105
FabianVarela <jfabian.303@gmail.com>
2025-12-26 19:31:55 -0500

    Update home using new radio button custom

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,4 +1,5 @@
-import 'package:collection/collection.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:weight_planet_calculator/hooks/curved_animation_hook.dart';
@@ -18,25 +19,25 @@ class HomeView extends HookWidget {
   Widget build(BuildContext context) {
     final formattedText = useState('');
     final currentIndex = useState(0);
-    final groupValue = useState(0);
 
+    final groupValue = useState<PlanetInfo?>(null);
     final textController = useTextEditingController();
 
     final animController = useAnimationController(
-      initialValue: 0.1,
+      initialValue: .1,
       duration: const Duration(milliseconds: 500),
     );
     final animation = useCurvedAnimation(animController);
 
     return Scaffold(
       body: SingleChildScrollView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        keyboardDismissBehavior: .onDrag,
         child: Column(
           spacing: 25,
           children: <Widget>[
             _Header(index: currentIndex.value),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const .symmetric(horizontal: 16),
               child: Column(
                 spacing: 20,
                 children: <Widget>[
@@ -51,7 +52,7 @@ class HomeView extends HookWidget {
                       formattedText.value,
                       style: TextStyle(
                         fontSize: 20,
-                        fontWeight: FontWeight.w700,
+                        fontWeight: .w700,
                         color: Theme.of(context).primaryColor,
                       ),
                     ),
@@ -59,20 +60,23 @@ class HomeView extends HookWidget {
                   _PlanetList(
                     planets: planets,
                     groupValue: groupValue.value,
-                    onChange: (radio, text, weight) {
-                      final res = _calculateWeight(textController.text, weight);
+                    onChange: (info, index) {
+                      final res = _calculateWeight(
+                        textController.text,
+                        info.value,
+                      );
 
                       if (animController.status == AnimationStatus.completed) {
-                        animController.reverse();
+                        unawaited(animController.reverse());
                       }
 
                       Future.delayed(const Duration(milliseconds: 500), () {
                         if (res != 0) {
-                          groupValue.value = radio;
-                          currentIndex.value = radio;
+                          groupValue.value = info;
+                          currentIndex.value = index;
                         }
-                        formattedText.value = _textResult(res, text);
-                        animController.forward();
+                        formattedText.value = _textResult(res, info.name);
+                        unawaited(animController.forward());
                       });
                     },
                   ),
@@ -96,7 +100,6 @@ class HomeView extends HookWidget {
 
   double _calculateWeight(String weight, double multiplier) {
     final weightValue = double.tryParse(weight);
-
     return weightValue != null && weightValue > 0
         ? ((double.parse(weight) * _poundValue) * multiplier) / _poundValue
         : 0;
@@ -110,6 +113,7 @@ class _Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final size = MediaQuery.sizeOf(context);
 
     return SizedBox(
@@ -118,21 +122,24 @@ class _Header extends StatelessWidget {
         children: <Widget>[
           ClipPath(
             clipper: HeaderClipPath(),
-            child: Container(
-              width: size.width,
-              height: size.height * .34,
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-              child: Align(
-                alignment: Alignment.topCenter,
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: theme.primaryColor),
+              child: SizedBox.fromSize(
+                size: Size(size.width, size.height * .34),
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: Text(
-                    'Weight Planet Calculator',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).colorScheme.surface,
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Align(
+                    alignment: .topCenter,
+                    child: Padding(
+                      padding: const .only(top: 100),
+                      child: Text(
+                        'Weight Planet Calculator',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: .w700,
+                          color: theme.colorScheme.surface,
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -156,36 +163,41 @@ class _Header extends StatelessWidget {
   }
 }
 
-typedef OnChangeList = void Function(int, String, double);
+typedef PlanetInfo = ({double value, String name});
+typedef OnChangeList = void Function(PlanetInfo, int);
 
 class _PlanetList extends HookWidget {
   const _PlanetList({
     required this.planets,
-    this.groupValue = 0,
+    required this.groupValue,
     this.onChange,
   });
 
   final List<Planet> planets;
-  final int groupValue;
+  final PlanetInfo? groupValue;
   final OnChangeList? onChange;
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      alignment: WrapAlignment.center,
-      children: planets.mapIndexed((index, item) {
-        final name = item.name;
-        final weightGravity = item.weightGravity;
+    return CustomRadioButtonGroup<PlanetInfo>(
+      groupList: [
+        for (final item in planets)
+          (
+            title: item.name,
+            value: (value: item.weightGravity, name: item.name),
+            color: item.color,
+          ),
+      ],
+      value: groupValue,
+      position: .wrap,
+      onChanged: (value) {
+        if (value == null) return;
 
-        return CustomRadioButton(
-          title: name,
-          value: index,
-          groupValue: groupValue,
-          weight: weightGravity,
-          color: item.color,
-          onChanged: (value) => onChange?.call(value!, name, weightGravity),
-        );
-      }).toList(),
+        final index = planets.indexWhere((item) {
+          return item.weightGravity == value.value && item.name == value.name;
+        });
+        onChange?.call(value, index);
+      },
     );
   }
 }
